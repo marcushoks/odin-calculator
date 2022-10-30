b4978a10089b9ed53f30838bdcc52e06a790a83c
marcushoks <marcushoks@gmail.com>
2022-10-30 17:49:30 +0800

    Add listener for number buttons to update the display

--- script.ts
+++ script.ts
@@ -1,3 +1,10 @@
+let input = "";
+
+const buttons = document.querySelectorAll(".button.number");
+buttons.forEach((button) => {
+  button.addEventListener("click", updateDisplay);
+});
+
 function add(a: number, b: number) {
   return a + b;
 }
@@ -16,4 +23,12 @@ function divide(a: number, b: number) {
 
 function operate(operator: Function, a: number, b: number): number {
   return operator(a, b);
+}
+
+function updateDisplay(e: Event) {
+  const buttonClicked = e.target as HTMLElement;
+  input += buttonClicked.textContent;
+
+  const display = document.querySelector("#display");
+  if (display) display.textContent = input;
 }
