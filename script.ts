a16ac01b7487b2b213f5ce9b5ab39fb690049103
marcushoks <marcushoks@gmail.com>
2022-12-04 16:35:29 +0800

    Add +, -, x, / functions

--- script.ts
+++ script.ts
@@ -1,9 +1,54 @@
-let input = "";
+let display = "";
 
-const buttons = document.querySelectorAll(".button.number");
-buttons.forEach((button) => {
-  button.addEventListener("click", updateDisplay);
+let operandA: number | null;
+let operandB: number | null;
+
+let operator: Function | null;
+
+const operandBtns = document.querySelectorAll(".button.operand");
+operandBtns.forEach((button) => {
+  button.addEventListener("click", updateOperand);
 });
+
+const operatorBtns = document.querySelectorAll(".button.operator");
+operatorBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (display.length === 0) return;
+
+    if (!operandA) {
+      operandA = parseFloat(display);
+      display = "";
+      updateDisplay(display);
+    }
+
+    const operatorId = (e.target as HTMLElement).id;
+
+    switch (operatorId) {
+      case "add": {
+        operator = add;
+        break;
+      }
+      case "subtract": {
+        operator = subtract;
+        break;
+      }
+      case "multiply": {
+        operator = multiply;
+        break;
+      }
+      case "divide": {
+        operator = divide;
+        break;
+      }
+    }
+  });
+});
+
+const evaluateBtn = document.querySelector("#evaluate");
+evaluateBtn?.addEventListener("click", evaluate);
+
+const clearEntryBtn = document.querySelector("#clear-entry");
+clearEntryBtn?.addEventListener("click", clearEntry);
 
 function add(a: number, b: number) {
   return a + b;
@@ -21,14 +66,45 @@ function divide(a: number, b: number) {
   return a / b;
 }
 
-function operate(operator: Function, a: number, b: number): number {
-  return operator(a, b);
+function updateOperand(e: Event) {
+  const input = (e.target as HTMLElement).textContent;
+
+  if (!input) return;
+
+  // prevent user from stacking 0's
+  if (display === "0" && input === "0") return;
+
+  // remove leading 0
+  if (display === "0") display = "";
+
+  display += input;
+  updateDisplay(display);
 }
 
-function updateDisplay(e: Event) {
-  const buttonClicked = e.target as HTMLElement;
-  input += buttonClicked.textContent;
+function evaluate() {
+  if (operandA && operator && display) {
+    operandB = parseFloat(display);
 
+    const result = operator(operandA, operandB);
+
+    display = String(result);
+    updateDisplay(display);
+
+    operandA = null;
+    operandB = null;
+    operator = null;
+  }
+}
+
+function clearEntry() {
+  operandA = null;
+  operandB = null;
+  operator = null;
+  display = "";
+  updateDisplay(display);
+}
+
+function updateDisplay(str: string) {
   const display = document.querySelector("#display");
-  if (display) display.textContent = input;
+  if (display) display.textContent = str;
 }
